b3e2f3da39b02c0189c77d21e0ffa0e1a2feacfc
Asís <asismelgarejo@gmail.com>
2021-01-18 12:37:59 -0500

    aff

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
 const categories = document.querySelectorAll('.categories__item h4');
-console.log(categories)
-categories.forEach(element => {
-  element.addEventListener('click',e=>{
-    const content = element.nextElementSibling;
-    if(content.style.maxHeight){
-      element.style.color = 'var(--color-main)';
-      element.style.background = 'var(--color-inverse-main)';
-      content.style.maxHeight = null;
-    }else{
-      element.style.color = 'var(--color-inverse-main)';
-      element.style.background = 'var(--color-main)';
-      content.style.maxHeight = content.scrollHeight+'px';
-    }
+
+function addFoldingMenu(){
+  
+if(document.documentElement.clientWidth <= 412){
+  categories.forEach(element => {
+    element.addEventListener('click',e=>{
+      const content = element.nextElementSibling;
+      if(content.style.maxHeight){
+        element.style.color = 'var(--color-main)';
+        element.style.background = 'var(--color-inverse-main)';
+        content.style.maxHeight = null;
+      }else{
+        element.style.color = 'var(--color-inverse-main)';
+        element.style.background = 'var(--color-main)';
+        content.style.maxHeight = content.scrollHeight+'px';
+      }
+    });
   });
-});
+}
+}
+addFoldingMenu();
